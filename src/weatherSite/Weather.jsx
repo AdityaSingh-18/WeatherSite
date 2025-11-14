5e1233063b0da3b06be29f3e123b2a6a4f36d73b
AdityaSingh <anshul8303858511@gmail.com>
2025-11-14 17:06:07 +0530

    useState to handle and display City details

---
+++ src/weatherSite/Weather.jsx
@@ -0,0 +1,17 @@
+export let weatherDetails = [
+    {
+        city: "Lucknow",
+        temperature: "26C",
+        description: "Sunny"
+    },
+    {
+        city: "Delhi",
+        temperature: "20C",
+        description: "Cloudy"
+    },
+    {
+        city: 'Chennai',
+        temperature: '36C',
+        description: 'Humid'
+    }
+];
--- src/weatherSite/WeatherSite.jsx
+++ src/weatherSite/WeatherSite.jsx
@@ -1,18 +1,39 @@
+import { useState } from 'react';
+import {weatherDetails} from './Weather.jsx';
 import './WeatherSite.css';
 
 export default function WeatherSite() {
-  return (
+
+    let [city, setCity] = useState("");
+    let [details, setDetails] = useState({});
+
+    let handleCity = (event) => {
+        event.preventDefault();
+        setCity(event.target.city.value);
+        handleDetails();
+    }
+
+    let handleDetails = () => {
+        let cityDetails = weatherDetails.find(v => v.city === city);
+        setDetails(cityDetails || {});
+    };
+
+    return (
     <>
-        <div className="weatherContainer">
-            <h1 className="weatherTitle">Weather Site</h1>
-            <form>
-                <input type="text" className="weatherInput"></input>
-                <button className="searchButton">Search</button> 
-            </form>
-            <div className="detailsContainer">
-                <h3>City</h3>
-                <p>Temp: 26C</p>
-                <p>Sunny</p>
+        <div className="wContainer">
+            <div className="wContent">
+                <h1 className="wTitle">Weather Site</h1>
+                <form className="wInputContainer" onSubmit={handleCity}>
+                    <input className="wInput" type="text" name="city"
+                     placeholder="Enter a City Name">
+                    </input>
+                    <button className="searchButton">Search</button>
+                </form> 
+                <div className="detailsContainer">
+                    <div className="wCity">{details.city}</div>
+                    <div className="wTemperature">{details.temperature}</div>
+                    <div className="wDescription">{details.description}</div>
+                </div>
             </div>
         </div>
     </>
